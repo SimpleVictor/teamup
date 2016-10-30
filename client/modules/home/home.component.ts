5808a21a55efc8b84d2c011fc3d719c9ca3b7ce5
VietAnhh <viet.le175@gmail.com>
2016-10-29 23:58:37 -0400

    added background

--- client/modules/home/home.component.ts
+++ client/modules/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "home",
+    styleUrls: [`client/modules/home/home.component.css`],
     templateUrl: `client/modules/home/home.component.html`
 })
 export class HomeComponent {
